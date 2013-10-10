c0547116dbb8eb68d294cbec4b83f63842024438
brunops <brunopsanches@gmail.com>
2013-10-10 12:01:05 -0700

    Point root_path to Questions#index

---
+++ spec/features/question_index_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature "Questions index (home) page" do
+  let(:question) { FactoryGirl.create (:question) }
+
+  scenario "shows question on home page" do
+    question
+    visit root_path
+    expect(page).to have_content question.body
+  end
+end
